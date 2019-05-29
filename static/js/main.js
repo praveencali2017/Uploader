8af1e0cda7d71d2273a4f8c3c4cfc578dd86f72a
Prav <praveenknjun12@gmail.com>
2019-05-28 22:37:12 -0700

    first check in

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+document.getElementById("file").onchange = function() {
+    document.getElementById("file_form").submit();
+};
+
+document.getElementById("search").onclick = function() {
+    document.getElementById("search_form").submit();
+};
+
+
